7cd0ab425810511ef88e714371728fd5c5df370b
Joshua Lynch <149664064+Joshieingold@users.noreply.github.com>
2024-06-06 02:43:49 -0300

    Add files via upload

--- Kattis Calculator/index.js
+++ Kattis Calculator/index.js
@@ -2,33 +2,46 @@
 let your_score = 0;
 let their_score = 0;
 let percent_complete = "";
-let one_four = "";
-let one_five = "";
-let one_six = "";
+let new_estimation = "";
+let estimate_x = 1.1
 
 // Get Information from HTML
 function HTMLGet() {
     your_score = (document.getElementById("your_score_find"));
     their_score = document.getElementById("their_score_find");
-    your_score = Number(your_score.value)
-    their_score = Number(their_score.value)
+    your_score = Number(your_score.value);
+    their_score = Number(their_score.value);
 }
 // Calculate Data
 function Calculate() {
     your_score = Number(your_score);
     their_score = Number(their_score);
     percent_complete = "You are " + Math.round(your_score / their_score * 100) + "% Complete";
-    one_four = "You need to solve " + Math.round((their_score - your_score) / 1.4) + " more 1.4 rated problems to achieve your goal.";
-    one_five = "You need to solve " + Math.round((their_score - your_score) / 1.5) + " more 1.5 rated problems to achieve your goal.";
-    one_six = "You need to solve " + Math.round((their_score - your_score) / 1.6) + " more 1.6 rated problems to achieve your goal.";
+    new_estimation = "You need to solve " + Math.round((their_score - your_score) / estimate_x) + " more " + (estimate_x).toFixed(1) + " rated problems to achieve your goal.";
+
 
 }
 // Update UI
 function UpdateUI() {
-    onefour_html.innerText = one_four;
-    onefive_html.innerText = one_five;
-    onesix_html.innerText = one_six;
-    percent_complete_html.innerText = percent_complete;
+    if (their_score === 0 || your_score === 0) {
+        return
+    }
+    else {
+        estimation.innerText = new_estimation;
+        percent_complete_html.innerText = percent_complete;
+    }
+}
+// Cycle Estimations
+function CycleUp() {
+    estimate_x += 0.1;
+    Calculate()
+    UpdateUI()
+}
+// Cycle Estimations
+function CycleDown() {
+    estimate_x -= 0.1;
+    Calculate()
+    UpdateUI()
 }
 
 // Run Program
